cce004bb2df62f5b99f18ec7986335fb29cd5f87
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-01 17:46:07 +0530

    style: adjust indentation for scanner initialization in D_searchNumberInArray

--- _9_Arrays/D_searchNumberInArray.java
+++ _9_Arrays/D_searchNumberInArray.java
@@ -2,7 +2,7 @@ package _9_Arrays;
 import java.util.Scanner;
 public class D_searchNumberInArray {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
             System.out.print("Enter the row:");
             int row=sc.nextInt();
             System.out.print("Enter the column:");
