20562a1237fa346d77df7f0ad532e0af5de49a81
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-01 17:54:01 +0530

    style: use try-with-resources for scanner initialization in B_searchANumber

--- _9_Arrays/B_searchANumber.java
+++ _9_Arrays/B_searchANumber.java
@@ -2,22 +2,22 @@ package _9_Arrays;
 import java.util.Scanner;
 public class  B_searchANumber{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the size of array:");
-        int size=sc.nextInt();
-        int[] number=new int[size];
-        for(int i=0;i<size;i++){
-            System.out.print("Enter the "+(i+1)+"th array:");
-            number[i]=sc.nextInt();
-        }
-        System.out.print("Enter the no for search:");
-        int x=sc.nextInt();
-        for(int i=0;i<number.length;i++){
-            if(number[i]==x){
-                System.out.print("The index of "+x+" is "+i);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the size of array:");
+            int size=sc.nextInt();
+            int[] number=new int[size];
+            for(int i=0;i<size;i++){
+                System.out.print("Enter the "+(i+1)+"th array:");
+                number[i]=sc.nextInt();
+            }
+            System.out.print("Enter the no for search:");
+            int x=sc.nextInt();
+            for(int i=0;i<number.length;i++){
+                if(number[i]==x){
+                    System.out.print("The index of "+x+" is "+i);
+                }
             }
         }
-        sc.close();
     }
     
 }
