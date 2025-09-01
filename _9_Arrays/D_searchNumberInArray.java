0c5630fc1072d1e0feb05947e005200f954db20c
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-01 17:45:14 +0530

    style: improve formatting and spacing in D_searchNumberInArray

--- _9_Arrays/D_searchNumberInArray.java
+++ _9_Arrays/D_searchNumberInArray.java
@@ -2,29 +2,29 @@ package _9_Arrays;
 import java.util.Scanner;
 public class D_searchNumberInArray {
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the row:");
-        int row=sc.nextInt();
-        System.out.print("Enter the column:");
-        int column=sc.nextInt();
-        
-        System.out.println("Enter the matrix:");
-        int[][] number=new int[row][column];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<column;j++){
-                number[i][j]=sc.nextInt();
-            }
-        } 
-        System.out.print("Enter the number for search:");
-        int x=sc.nextInt();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<column;j++){
-                if(number[i][j]==x){
-                    System.out.print("The row is "+i+" and the column is "+j);
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the row:");
+            int row=sc.nextInt();
+            System.out.print("Enter the column:");
+            int column=sc.nextInt();
+            
+            System.out.println("Enter the matrix:");
+            int[][] number=new int[row][column];
+            for(int i=0;i<row;i++){
+                for(int j=0;j<column;j++){
+                    number[i][j]=sc.nextInt();
                 }
             }
-            System.out.println();
-        } 
-        sc.close();
+            System.out.print("Enter the number for search:");
+            int x=sc.nextInt();
+            for(int i=0;i<row;i++){
+                for(int j=0;j<column;j++){
+                    if(number[i][j]==x){
+                        System.out.print("The row is "+i+" and the column is "+j);
+                    }
+                }
+                System.out.println();
+            }
+            sc.close();
     }
 }
