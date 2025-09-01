4a93b7f95095fa78e0ebf133d00bd8f1724d2a40
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-01 16:57:48 +0530

    remove greeting message from main method

--- App.java
+++ App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
     System.out.println("Hello, World!");
     System.out.println("This is a sample Java application.");
-    System.out.println("hello user");
     }
 }
