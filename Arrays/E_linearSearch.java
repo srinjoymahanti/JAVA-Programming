ad8514d3ce056168e5bc80c256586b602dfed3ce
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-12 16:30:23 +0530

    feat: implement linear search functionality and array input in E_linearSearch class chore: add .gitignore and project configuration files

--- Arrays/E_linearSearch.java
+++ Arrays/E_linearSearch.java
@@ -1,6 +1,16 @@
 package Arrays;
+import java.util.*;
 public class E_linearSearch {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int n=sc.nextInt();
+        int[] arr= new int[];
+        System.out.print("Enter array elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter the number to search:");
         
     }
     
