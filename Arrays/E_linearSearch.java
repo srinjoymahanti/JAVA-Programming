db683a97ea5b0bffa5d6a56af1080453a821ad8b
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-12 17:24:21 +0530

    Merge remote-tracking branch 'origin/main'

--- Arrays/E_linearSearch.java
+++ Arrays/E_linearSearch.java
@@ -11,7 +11,12 @@ public class E_linearSearch {
             arr[i]=sc.nextInt();
         }
         System.out.print("Enter the number to search:");
-        
+        int x=sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            if(arr[i]==x) System.out.println("Number found");
+            else System.out.println("Number not found");
+        }
     }
     
 }
