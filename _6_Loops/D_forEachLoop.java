8261dad049b3f11daeefd1a361d2f74f41764fad
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-13 12:16:33 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ _6_Loops/D_forEachLoop.java
@@ -0,0 +1,10 @@
+package _6_Loops;
+
+public class D_forEachLoop {
+    public static void main(String[] args) {
+        int[] arr={12,34,56,78,90,81,92,73,64,50};
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
