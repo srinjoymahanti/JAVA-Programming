62f381b952c0c0df1707089bfebd61ddf27b3d89
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-13 16:22:09 +0530

    Merge remote-tracking branch 'origin/main'

--- _13_ArrayList/_1_basics.java
+++ _13_ArrayList/_1_basics.java
@@ -1,9 +1,9 @@
 package _13_ArrayList;
 import java.util.ArrayList;
 import java.util.Collections;
-public class _1_addElements{
-    public static void main(String args[]){
-        ArrayList<Integer> list=new ArrayList<Integer>();
+public class _1_basics {
+    public static void main(String[] args){
+        ArrayList<Integer> list=new ArrayList<>();
 
         //Add elements
         list.add(23);
@@ -12,7 +12,7 @@ public class _1_addElements{
         System.out.println(list);
 
         //Get elements
-        int element = list.get(0);
+        int element = list.get(1);
         System.out.println(element);
 
         //Add elements in between
@@ -34,7 +34,7 @@ public class _1_addElements{
         //loops
         for(int i=0;i<list.size();i++){
             System.out.print(list.get(i)+" ");
-        };
+        }
         System.out.println();
 
         //Sorting
