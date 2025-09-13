d0c518240e1d0f09ed1b4b746002583ff78f6d12
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-13 10:34:39 +0530

    Merge remote-tracking branch 'origin/main'

--- _9_Arrays/F_maxFromArray.java
+++ _9_Arrays/F_maxFromArray.java
@@ -1,7 +1,26 @@
 package _9_Arrays;
-
+import java.util.*;
 public class F_maxFromArray {
     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
 
+        System.out.print("Enter the size of the array:");
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the element of the array:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int max=arr[0];
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                index=i;
+            }
+        }
+        System.out.println("The maximum number is:"+max+" at index "+index);
     }
 }
