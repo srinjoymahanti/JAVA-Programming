1afcbbd24ac5415f56c7d5f3f571f6dac559bc47
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-14 20:40:02 +0530

    Merge remote-tracking branch 'origin/main'

--- _9_Arrays/_11_sort0sand1s.java
+++ _9_Arrays/_11_sort0sand1s.java
@@ -3,5 +3,19 @@ package _9_Arrays;
 public class _11_sort0sand1s {
     public static void main(String[] args) {
         int[] arr={0,0,0,1,0,1,1,1,0,1};
+        int n=arr.length;
+        int noOfZeros=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0) noOfZeros++;
+        }
+        for(int i=0;i<noOfZeros-1;i++){
+            arr[i]=0;
+        }
+        for(int i=noOfZeros;i<n;i++){
+            arr[i]=1;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
