182f6dc7c5ae8767a4f338a53e38895c8ede6fb9
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-14 17:18:06 +0530

    Merge remote-tracking branch 'origin/main'

--- _9_Arrays/_10_reverseFromGivenIndex.java
+++ _9_Arrays/_10_reverseFromGivenIndex.java
@@ -1,10 +1,16 @@
 package _9_Arrays;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class _10_reverseFromGivenIndex {
     public static void reverse(int[] array,int a,int b){
-
+        while(a<=b){
+            int temp=array[a];
+            array[a]=array[b];
+            array[b]=temp;
+            a++;
+            b--;
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,6 +21,14 @@ public class _10_reverseFromGivenIndex {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        System.out.println("Enter the index to reverse");
+        int k=sc.nextInt();
 
+        reverse(arr,0,n-k-1);
+        reverse(arr,n-k,n-1);
+        reverse(arr,0,n-1);
+        for(int ele:arr){
+            System.out.print(arr[ele]);
+        }
     }
 }
