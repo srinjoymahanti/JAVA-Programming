43b83f2ac7cd984b2823e582bd79431c00fb8676
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-16 00:39:00 +0530

    Merge remote-tracking branch 'origin/main'

--- _9_Arrays/_12_sort0sand1sand2s.java
+++ _9_Arrays/_12_sort0sand1sand2s.java
@@ -4,16 +4,19 @@ public class _12_sort0sand1sand2s {
     public static void main(String[] args) {
         int[] arr={0,0,1,0,2,1,0,2,2,1};
         int n=arr.length;
-        int noOfZeros=0,noOfOnes=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==0) noOfZeros++;
-            if(arr[i]==1) noOfOnes++;
-        }
-        for(int i=0;i<n;i++){
-            if(i<noOfZeros) arr[i]=0;
-            else if(i<noOfZeros+noOfOnes) arr[i]=1;
-            else arr[i]=2;
-        }
+
+//        //M-1
+//        int noOfZeros=0,noOfOnes=0;
+//        for(int i=0;i<n;i++){
+//            if(arr[i]==0) noOfZeros++;
+//            if(arr[i]==1) noOfOnes++;
+//        }
+//        for(int i=0;i<n;i++){
+//            if(i<noOfZeros) arr[i]=0;
+//            else if(i<noOfZeros+noOfOnes) arr[i]=1;
+//            else arr[i]=2;
+//        }
+
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
