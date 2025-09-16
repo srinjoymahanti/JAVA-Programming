b4804a7245528f5511b39cf0aa87d3fd77d5bad2
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-16 17:36:39 +0530

    Merge remote-tracking branch 'origin/main'

--- _9_Arrays/_14_nextGreatestElement.java
+++ _9_Arrays/_14_nextGreatestElement.java
@@ -6,13 +6,23 @@ public class _14_nextGreatestElement {
         int n=arr.length;
         int[] ans=new int[n];
         ans[n-1]=-1;
-        for(int i=0;i<n-1;i++){
-            int max=Integer.MIN_VALUE;
-            for(int j=i+1;j<n;j++){
-                max=Math.max(max,arr[j]);
-            }
-            ans[i]=max;
+
+//        //M-1:Brute Force
+//        for(int i=0;i<n-1;i++){
+//            int max=Integer.MIN_VALUE;
+//            for(int j=i+1;j<n;j++){
+//                max=Math.max(max,arr[j]);
+//            }
+//            ans[i]=max;
+//        }
+
+        //M-2: Optimised
+        int nge=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans[i]=nge;
+            nge=Math.max(nge,arr[i]);
         }
+
         for(int ele:ans){
             System.out.print(ele+" ");
         }
