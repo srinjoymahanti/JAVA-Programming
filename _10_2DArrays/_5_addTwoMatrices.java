903798f637cd8ee27b36c64e7f1f4e4dddad8287
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-19 01:03:44 +0530

    Merge remote-tracking branch 'origin/main'

--- _10_2DArrays/_5_addTwoMatrices.java
+++ _10_2DArrays/_5_addTwoMatrices.java
@@ -6,17 +6,31 @@ public class _5_addTwoMatrices {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int[][] arr=new int[3][3];
+        int[][] brr=new int[3][3];
+        int[][] crr=new int[3][3];
         System.out .println("Enter the 3X3 first array:");
-        for(int i=0;i<4;i++){
+        for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 arr[i][j]=sc.nextInt();
             }
         }
         System.out .println("Enter the 3X3 second array:");
-        for(int i=0;i<4;i++){
+        for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                arr[i][j]=sc.nextInt();
+                brr[i][j]=sc.nextInt();
             }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                crr[i][j]=arr[i][j]+brr[i][j];
+            }
+        }
+        System.out.println("The final array is:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(crr[i][j]+" ");
+            }
+            System.out.println();
         }
 
     }
