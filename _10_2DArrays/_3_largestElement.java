c7c5b94465a62cd873af8ce59a401d50726ddeb3
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-19 00:34:47 +0530

    Merge remote-tracking branch 'origin/main'

--- _10_2DArrays/_3_largestElement.java
+++ _10_2DArrays/_3_largestElement.java
@@ -1,4 +1,23 @@
 package _10_2DArrays;
 
+import java.util.Scanner;
+
 public class _3_largestElement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] arr=new int[4][2];
+        System.out .println("Enter the array:");
+        for(int i=0;i<4;i++){
+            for(int j=0;j<2;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                max=Math.max(arr[i][j],max);
+            }
+        }
+        System.out.println(max);
+    }
 }
