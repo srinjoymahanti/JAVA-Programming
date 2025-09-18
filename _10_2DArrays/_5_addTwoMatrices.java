1bfa74b479c273f650b4cca4aa9954d58b4fd02f
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-19 00:56:21 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ _10_2DArrays/_5_addTwoMatrices.java
@@ -0,0 +1,17 @@
+package _10_2DArrays;
+
+import java.util.Scanner;
+
+public class _5_addTwoMatrices {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] arr=new int[4][2];
+        System.out .println("Enter the array:");
+        for(int i=0;i<4;i++){
+            for(int j=0;j<2;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+    }
+}
