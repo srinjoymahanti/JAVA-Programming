2808d56c6ea32b8b94541f3722bc29a23f4c11e6
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-20 12:15:55 +0530

    Merge remote-tracking branch 'origin/main'

--- _10_2DArrays/_8_printingInWaveForm.java
+++ _10_2DArrays/_8_printingInWaveForm.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class _8_printingInWaveForm {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int[][] arr=new int[3][3];
+        int[][] arr=new int[4][2];
         int m=arr.length;
         int n=arr[0].length;
         System.out .println("Enter the array:");
@@ -14,16 +14,38 @@ public class _8_printingInWaveForm {
                 arr[i][j]=sc.nextInt();
             }
         }
-        System.out.println("Printing in wave form:");
-        for(int i=0;i<m;i++){
+//        //row wise printing
+//        System.out.println("Printing in wave form:");
+//        for(int i=0;i<m;i++){
+//            if(i%2==0){
+//                for(int j=0;j<n;j++){
+//                    System.out.print(arr[i][j]+" ");
+//                }
+//            }
+//            else{
+//                for(int j=n-1;j>=0;j--){
+//                    System.out.print(arr[i][j]+" ");
+//                }
+//            }
+//        }
+
+        //column wise printing
+        int[][] transpose=new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                transpose[i][j]=arr[j][i];
+            }
+        }
+        System.out.println("Printing in column wise wave form:");
+        for(int i=0;i<n;i++){
             if(i%2==0){
-                for(int j=0;j<n;j++){
-                    System.out.print(arr[i][j]+" ");
+                for(int j=0;j<m;j++){
+                    System.out.print(transpose[i][j]+" ");
                 }
             }
             else{
-                for(int j=n-1;j>=0;j--){
-                    System.out.print(arr[i][j]+" ");
+                for(int j=m-1;j>=0;j--){
+                    System.out.print(transpose[i][j]+" ");
                 }
             }
         }
