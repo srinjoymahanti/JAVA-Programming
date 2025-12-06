90bfb9b73549f788d03c1288fb375b4b2828609f
Srinjoy <srinjoymahanti@gmail.com>
2025-12-06 22:55:46 +0530

    Merge remote-tracking branch 'origin/main'

--- _11_OOPS/test.java
+++ _11_OOPS/test.java
@@ -1,21 +1,38 @@
 package _11_OOPS;
 
+//class A{
+//
+//    void show(){
+//        System.out.println("hello");
+//    }
+//}
+//class B extends A{
+//
+//    void show(){
+//        System.out.println("cello");
+//    }
+//}
+//
+//class test{
+//    public static void main(String[] args) {
+//        A r=new B();
+//        r.show();
+//    }
+//}
+
 class A{
-
-    void show(){
-        System.out.println("hello");
+    private int value;
+    public void setValue(int x){
+        value=x;
+    }
+    public int getValue(){
+        return value;
     }
 }
-class B extends A{
-
-    void show(){
-        System.out.println("cello");
-    }
-}
-
-class test{
+class B{
     public static void main(String[] args) {
-        A r=new B();
-        r.show();
+        A r=new A();
+        r.setValue(100);
+        System.out.println(r.getValue());
     }
 }
