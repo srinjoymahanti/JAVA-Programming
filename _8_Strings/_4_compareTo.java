2f01b8ab8de5daa4c148874bc8f6517ceabeac8f
Srinjoy <srinjoymahanti@gmail.com>
2025-12-10 10:11:59 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ _8_Strings/_4_compareTo.java
@@ -0,0 +1,9 @@
+package _8_Strings;
+
+public class _4_compareTo {
+    public static void main(String[] args) {
+        String a="abcdd";
+        String b="azc";
+        System.out.println(a.compareTo(b));
+    }
+}
--- _8_Strings/_5_concatenation.java
+++ _8_Strings/_5_concatenation.java
@@ -1,9 +1,12 @@
 package _8_Strings;
-public class A_concatenation{
+public class _5_concatenation {
     public static void main(String[] args){
         String firstName="tony";
         String lastName="stark";
         String fullName=firstName+"@"+lastName;
         System.out.println(fullName);
+        System.out.println(firstName.concat(lastName));
+        System.out.println(firstName.concat(firstName));
+        System.out.println(lastName.concat(firstName));
     }
 }
