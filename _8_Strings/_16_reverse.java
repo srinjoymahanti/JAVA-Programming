502d93d109c465b7ecafb8c900d08c10141a5cb6
Srinjoy <srinjoymahanti@gmail.com>
2025-12-23 00:20:05 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ _8_Strings/_16_reverse.java
@@ -0,0 +1,10 @@
+package _8_Strings;
+
+import java.util.Scanner;
+public class _16_reverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder(sc.nextLine());
+
+    }
+}
