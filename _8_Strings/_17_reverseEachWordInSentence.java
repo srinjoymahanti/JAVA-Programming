a23a2ea237d8439cd0e099f37ad052a897aadc6a
Srinjoy <srinjoymahanti@gmail.com>
2025-12-23 13:21:07 +0530

    Merge remote-tracking branch 'origin/main'

--- _8_Strings/_17_reverseEachWordInSentence.java
+++ _8_Strings/_17_reverseEachWordInSentence.java
@@ -1,7 +1,30 @@
 package _8_Strings;
 
+import java.util.Scanner;
+
 public class _17_reverseEachWordInSentence {
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder(sc.nextLine());
+        int n=sb.length();
+        int i=0,j=0;
+        while(j<n){
+            if(sb.charAt(j) != ' ') j++;
+            else{
+                reverse(sb,i,j-1);
+                i=j+1;
+                j=i;
+            }
+        }
+        reverse(sb,i,j-1);//for last word reverse
+        System.out.println(sb);
+    }
+    public static void reverse(StringBuilder sb,int i,int j){
+        while(i<=j){
+            char temp= sb.charAt(i);
+            sb.setCharAt(i,sb.charAt(j));
+            sb.setCharAt(j,temp);
+            i++;j--;
+        }
     }
 }
