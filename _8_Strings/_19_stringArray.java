15da93fdf58c08517c4b83d64aca10431979da8a
Srinjoy <srinjoymahanti@gmail.com>
2025-12-23 22:06:07 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ _8_Strings/_19_stringArray.java
@@ -0,0 +1,10 @@
+package _8_Strings;
+
+public class _19_stringArray {
+    public static void main(String[] args) {
+        String[] str={"srinjoy","arya","mahanti","xyz"};
+        System.out.println(str[0]);
+        System.out.println(str[1]);
+        System.out.println(str[3]);
+    }
+}
