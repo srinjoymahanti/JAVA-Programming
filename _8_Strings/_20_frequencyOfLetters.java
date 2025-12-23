ccf5515cfc2b7636b338ed7f5d829b7bbd50374a
Srinjoy <srinjoymahanti@gmail.com>
2025-12-23 22:10:03 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ _8_Strings/_20_frequencyOfLetters.java
@@ -0,0 +1,14 @@
+package _8_Strings;
+
+import java.util.Scanner;
+
+public class _20_frequencyOfLetters {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int[] freq=new int[26];
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+    }
+}
