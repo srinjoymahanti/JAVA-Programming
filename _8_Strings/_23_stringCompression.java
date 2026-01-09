b25adaeceeb5f16bd3c815c6142fe1525c1a4b04
Srinjoy <srinjoymahanti@gmail.com>
2026-01-09 22:24:10 +0530

    updating

--- _8_Strings/_23_stringCompression.java
+++ _8_Strings/_23_stringCompression.java
@@ -2,22 +2,28 @@ package _8_Strings;
 
 public class _23_stringCompression {
     public static void main(String[] args) {
-        String s="aaaabbcccdeefff";
-        char[] crr=s.toCharArray();
-        String ans="";
+//        String s="aaaabbcccdeefff";
+//        char[] crr=s.toCharArray();
+        StringBuilder sb=new StringBuilder("aaaabbcccdeefff");
+        char[] crr=sb.toString().toCharArray();
+        StringBuilder ans=new StringBuilder("");
         int i=0,j=0;
-        while(j<s.length()){
+        while(j<crr.length){
             if(crr[i]==crr[j]) j++;
             else{
-                ans+=crr[i];
+//                ans+=crr[i];
+//                int len=j-i;
+//                if(len>1) ans+=len;
+//                i=j;
+                ans.append(crr[i]);
                 int len=j-i;
-                if(len>1) ans+=len;
+                if(len>1) ans.append(len);
                 i=j;
             }
         }
-            ans+=crr[i];
+            ans.append(crr[i]);
             int len=j-i;
-            if(len>1) ans+=len;
+            if(len>1) ans.append(len);
 
 
         System.out.print(ans);
