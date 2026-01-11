22c2beaa4e3b2533104dd362a659073eeda12e7f
Srinjoy <srinjoymahanti@gmail.com>
2026-01-11 22:38:12 +0530

    optimised bubble sort

--- _14_BasicSorting/_3_BubbleSort.java
+++ _14_BasicSorting/_3_BubbleSort.java
@@ -2,25 +2,27 @@ package _14_BasicSorting;
 
 public class _3_BubbleSort {
     public static void main(String[] args) {
-        int[] arr={5,4,3,2,1};
-        int n=arr.length;
-        for(int ele:arr){
-            System.out.print(ele+" ");
+        int[] arr = {1, 2, 4, 5, 3};
+        int n = arr.length;
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1-i;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+        for (int i = 0; i < n - 1; i++) {
+            boolean flag = true;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = false;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
+            if (flag==true) break;
             System.out.println();
             System.out.print(i+1+"pass-->");
             for(int ele:arr){
                 System.out.print(ele+" ");
             }
         }
-
     }
 }
