544e5292b7239034e3825d1dc374ae6b88e933ad
Srinjoy <srinjoymahanti@gmail.com>
2026-01-11 23:28:37 +0530

    optimized the code

--- _14_BasicSorting/_4_pushZerosToEnd.java
+++ _14_BasicSorting/_4_pushZerosToEnd.java
@@ -4,20 +4,19 @@ public class _4_pushZerosToEnd {
     public static void main(String[] args) {
         int[] arr={1,0,4,0,2,-40,100,0,5};
         int n=arr.length;
+        int noz=0;
         for(int ele:arr){
+            if(ele==0) noz++;
             System.out.print(ele+" ");
         }
-        for (int i = 0; i < n-1; i++) {
-            boolean flag=true;
+        for (int i = 0; i < noz; i++) {
             for (int j = 0; j < n-1; j++) {
                 if(arr[j]==0){
-                    flag=false;
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
                 }
             }
-            if(flag==true) break;
             System.out.println();
             System.out.print(i+1+"pass-->");
             for(int ele:arr){
