111d05d3a71867075e30fafab75fb8eabb83a727
Srinjoy <srinjoymahanti@gmail.com>
2026-01-13 11:35:50 +0530

    sort in decreasing order by bubble sort

---
+++ _14_BasicSorting/_6_decreasingBubbleSort.java
@@ -0,0 +1,23 @@
+package _14_BasicSorting;
+
+public class _6_decreasingBubbleSort {
+    public static void main(String[] args) {
+        int[] arr={2,9,4,7,11,6};
+        int n=arr.length;
+        for (int i = 0; i < n-1; i++) {
+            boolean flag=true;
+            for (int j = 0; j < n-1-i; j++) {
+                if(arr[j]<arr[j+1]) {
+                    flag = false;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            if(flag==true) break;
+        }
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
