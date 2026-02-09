b93df3fc3fd3d7a7e9610f33da350afcfb012172
Srinjoy <srinjoymahanti@gmail.com>
2026-02-09 12:14:49 +0530

    function calls

--- _16_Recursion/_1_functionCalls.java
+++ _16_Recursion/_1_functionCalls.java
@@ -1,4 +1,19 @@
 package _16_Recursion;
 
 public class _1_functionCalls {
+    public static void mango(){
+        System.out.println("I am in mango method");
+    }
+    public static void banana(){
+        System.out.println("I am in banana method");
+        mango();
+    }
+    public static void apple(){
+        System.out.println("I am in apple method");
+        banana();
+    }
+    public static void main(String[] args) {
+        System.out.println("I am in main method");
+        apple();
+    }
 }
