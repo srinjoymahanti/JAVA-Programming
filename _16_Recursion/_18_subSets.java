4f6c751ceb1cfc921743d3aab9948b3f7fc28b0c
Srinjoy <srinjoymahanti@gmail.com>
2026-02-11 15:44:20 +0530

    sub sets

---
+++ _16_Recursion/_18_subSets.java
@@ -0,0 +1,22 @@
+package _16_Recursion;
+
+import java.util.ArrayList;
+
+public class _18_subSets {
+    static ArrayList<String> list=new ArrayList<>();
+    public static void subSets(int i,String str,String ans) {
+        if(i==str.length()){
+            list.add(ans);
+            return;
+        }
+        char ch=str.charAt(i);
+        subSets(i+1,str,ans+ch);//take
+        subSets(i+1,str,ans);//not take
+
+    }
+    public static void main(String[] args) {
+        String s="abc";
+        subSets(0,s,"");
+        System.out.println(list);
+    }
+}
