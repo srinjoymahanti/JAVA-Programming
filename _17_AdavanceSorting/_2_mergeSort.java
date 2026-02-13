0bf65d2e03ccbdb431a3883c8932d42964f02c12
Srinjoy <srinjoymahanti@gmail.com>
2026-02-13 23:16:07 +0530

    merge sort

---
+++ _17_AdavanceSorting/_2_mergeSort.java
@@ -0,0 +1,41 @@
+package _17_AdavanceSorting;
+
+public class _2_mergeSort {
+    public static void print(int[] array){
+        for(int ele:array){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+    public static void merge(int[] a,int[] b,int[] c){
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]) c[k++] = a[i++];
+            else c[k++] = b[j++];
+        }
+        while(i<a.length) c[k++]=a[i++];
+        while(j<b.length) c[k++]=b[j++];
+    }
+    public static void mergeSort(int[] array){
+        int n=array.length;
+        if(n==1) return;
+        int[] a=new int[n/2];
+        int[] b=new int[n-n/2];
+        for(int i=0;i<a.length;i++){
+            a[i]=array[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            b[i]=array[n/2+i];
+        }
+        mergeSort(a);
+        mergeSort(b);
+
+        merge(a,b,array);
+    }
+    public static void main(String[] args){
+        int[] arr={19,78,23,69,45,59,37};
+        print(arr);
+        mergeSort(arr);
+        print(arr);
+    }
+}
