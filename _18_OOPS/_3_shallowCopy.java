2e1981955e7a136a0e2b3c6ebe4ba09805c4d54e
Srinjoy <srinjoymahanti@gmail.com>
2026-02-24 16:37:46 +0530

    static and final keyword

--- _18_OOPS/_3_shallowCopy.java
+++ _18_OOPS/_3_shallowCopy.java
@@ -1,20 +1,41 @@
 package _18_OOPS;
 
-public class _3_shallowCopy {
-    public static class Pokemon{
-        String name;
-        String type;
-        int speed;
-        float power;
-        boolean fly;
+class Pokemon{
+    static String creature;
+    private String name;
+    String type;
+    int speed;
+    float power;
+    boolean fly;
+    Pokemon(){
+
     }
+    Pokemon(String name){
+        //This parameterised constructor also acts as setter
+        this.name=name;
+    }
+    void setName(String name){//setter
+        this.name=name;
+    }
+    String getName(){//getter
+        return this.name;
+    }
+}
+public class _3_shallowCopy {
+
     public static void main(String[] args){
-        Pokemon p1=new Pokemon();
-        p1.name="pikachu";p1.type="electric";p1.speed=70;p1.power=50;p1.fly=false;
-        Pokemon p2 =new Pokemon();
-        p2.name="charizord";p2.type="fire";p2.speed=110;p2.power=90;p2.fly=true;
-        Pokemon p3=p1;//creating shallow copy
-        p3.name="eve";//it changes the p1.name
-        System.out.print(p1.name);
+        Pokemon p1=new Pokemon("pikachu");
+        p1.type="electric";p1.speed=70;p1.power=50.22f;p1.fly=false;
+        p1.creature="pokemon";
+
+        Pokemon p2 =new Pokemon("charizord");
+        p2.type="fire";p2.speed=110;p2.power=90.56f;p2.fly=true;
+        p2.creature="doreamon";
+
+        Pokemon p3=p1;
+        p3.setName("eve");
+
+        System.out.println(p1.getName());
+        System.out.println(p1.creature);
     }
 }
