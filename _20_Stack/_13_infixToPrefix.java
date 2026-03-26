69d813a838314a493518847a58e971e0612c75bd
Srinjoy <srinjoymahanti@gmail.com>
2026-03-26 16:33:03 +0530

    infix to prefix

---
+++ _20_Stack/_13_infixToPrefix.java
@@ -0,0 +1,54 @@
+package _20_Stack;
+
+import java.util.Stack;
+
+public class _13_infixToPrefix {
+    public static void main(String[] args) {
+        String infix="9-(5+3)*4/6";
+        Stack<String> val=new Stack<>();
+        Stack<Character> op=new Stack<>();
+        for (int i = 0; i < infix.length(); i++) {
+          char ch=infix.charAt(i);
+          int ascii=(int)ch;
+          if(ascii>=48 && ascii<=57) val.push(ch+"");
+          else if (op.isEmpty() || ch=='(' || op.peek()=='(') {
+              op.push(ch);
+          }
+          else if (ch==')') {
+              while(op.peek()!='('){
+                  String v2=val.pop();
+                  String v1=val.pop();
+                  char o=op.pop();
+                  val.push(o+v1+v2);
+              }
+              op.pop();//remove the '('
+          }
+          else{
+              if(ch=='+' || ch=='-'){
+                  String v2=val.pop();
+                  String v1=val.pop();
+                  char o=op.pop();
+                  val.push(o+v1+v2);
+                  op.push(ch);
+              }
+              if(ch=='*' || ch=='/'){
+                  if(op.peek()=='*' || op.peek()=='/'){
+                      String v2=val.pop();
+                      String v1=val.pop();
+                      char o=op.pop();
+                      val.push(o+v1+v2);
+                      op.push(ch);
+                  }
+                  else op.push(ch);
+              }
+          }
+        }
+        while(val.size()>1){
+            String v2=val.pop();
+            String v1=val.pop();
+            char o=op.pop();
+            val.push(o+v1+v2);
+        }
+        System.out.println(val.peek());
+    }
+}
