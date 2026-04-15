a65fab58869a100a3f4566112b0f163770c5b408
Srinjoy <srinjoymahanti@gmail.com>
2026-04-15 21:04:25 +0530

    Double Ended Queue

---
+++ _21_Queue/_7_deque.java
@@ -0,0 +1,18 @@
+package _21_Queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class _7_dequeue {
+    public static void main(String[] args) {
+        Deque dq=new LinkedList<>();
+        dq.addLast(23);
+        dq.addLast(12);
+        dq.addLast(76);
+        dq.addFirst(47);
+        System.out.println(dq);
+        dq.removeFirst();
+        dq.removeLast();
+        System.out.println(dq);
+    }
+}
